cd7c34b92951e10840e0befacc57e960741dc776
jcastilloro <jcastillorro@hotmail.com>
2025-08-20 02:53:02 -0500

    Correguir error en program agregar configuracion AddHttpContextAccessor y borrado de la clase WeatherForecast

--- SIGC.Presentation.WebApi/Program.cs
+++ SIGC.Presentation.WebApi/Program.cs
@@ -10,7 +10,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddHttpContextAccessor();
 builder.Services.AddSIGCCoreApplicationService();
 builder.Services.AddSIGCInfrastructureGeneralService();
 
